d9f342057f7bc080611c914b204d3cafcbbec2f7
Igor <igoralbieri04@gmail.com>
2024-10-08 13:40:09 -0400

    a

--- App/Db/connPoo.php
+++ App/Db/connPoo.php
@@ -5,10 +5,9 @@ $dbUsername = "teste";
 $dbPassword = "H3@LTH_2024";
 $dbName = "intra_health";
 
-// Conectar ao banco de dados
+
 $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
-// Verificar se a conexão foi bem-sucedida
 if ($db->connect_error) {
     die("Falha na conexão: " . $db->connect_error);
 }
