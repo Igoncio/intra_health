495ec1901e18a0b4dc4cdecac6bcb0850951b088
Igor <igoralbieri04@gmail.com>
2024-10-08 13:16:06 -0400

    a

--- includes/php/permissoes.php
+++ includes/php/permissoes.php
@@ -2,7 +2,7 @@
 
 include '.././App/Db/connPoo.php';
 
-$db = new PDO("mysql:host=localhost;dbname=intra_health", "root", "");
+$db = new PDO("mysql:host=localhost;dbname=intra_health", "teste", "H3@LTH_2024");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
